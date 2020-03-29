9a1958647b6c665b0d930268348114cba9793635
Ole Saether <osaether@gmail.com>
2020-03-29 19:48:15 +0200

    Added Dockerfile and docker_entrypoint.sh

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM openthread/otbr:latest
+
+ENV MQTT_SN_REPO=paho.mqtt-sn.embedded-c
+ENV MQTT_SN_ZIP=https://github.com/eclipse/"$MQTT_SN_REPO"/archive/develop.zip
+
+RUN DEBIAN_FRONTEND=noninteractive apt -y update
+RUN DEBIAN_FRONTEND=noninteractive apt -y install wget unzip libssl-dev
+
+RUN wget --progress=dot:giga --no-check-certificate -O "$MQTT_SN_REPO".zip "$MQTT_SN_ZIP"
+RUN unzip "$MQTT_SN_REPO".zip
+RUN rm "$MQTT_SN_REPO".zip
+
+RUN cd "$MQTT_SN_REPO"-develop/MQTTSNGateway && sed -i "s/^BrokerName=.*$/BrokerName=137.135.83.217/" gateway.conf
+RUN cd "$MQTT_SN_REPO"-develop/MQTTSNGateway && sed -i "s/^GatewayUDP6Hops=.*$/GatewayUDP6Hops=64/" gateway.conf
+RUN cd "$MQTT_SN_REPO"-develop/MQTTSNGateway && sed -i "s/^GatewayUDP6Port=.*$/GatewayUDP6Port=47193/" gateway.conf
+RUN cd "$MQTT_SN_REPO"-develop/MQTTSNGateway && sed -i "s/^GatewayUDP6Broadcast=.*$/GatewayUDP6Broadcast=ff33:40:fdde:ad00:beef:0:0:1/" gateway.conf
+RUN cd "$MQTT_SN_REPO"-develop/MQTTSNGateway && make SENSORNET=udp6 && make install
+
+ADD docker_entrypoint.sh /app/borderrouter/script
---
+++ docker_entrypoint.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+#
+#  Copyright (c) 2018, The OpenThread Authors.
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#  1. Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#  2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#  3. Neither the name of the copyright holder nor the
+#     names of its contributors may be used to endorse or promote products
+#     derived from this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+#  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+#  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+#  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+#  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+#  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#  POSSIBILITY OF SUCH DAMAGE.
+#
+
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+
+function parse_args()
+{
+    while [ $# -gt 0 ]
+    do
+        echo $1
+        case $1 in
+        --ncp-path)
+            NCP_PATH=$2
+            shift
+            shift
+            ;;
+        --interface|-I)
+            TUN_INTERFACE_NAME=$2
+            shift
+            shift
+            ;;
+        --nat64-prefix)
+            NAT64_PREFIX=$2
+            shift
+            shift
+            ;;
+        --disable-default-prefix-route)
+            AUTO_PREFIX_ROUTE=false
+            shift
+            ;;
+        --disable-default-prefix-slaac)
+            AUTO_PREFIX_SLAAC=false
+            shift
+            ;;
+        --panid)
+            NETWORK_PANID=$2
+            shift
+            shift
+            ;;
+        --xpanid)
+            NETWORK_XPANID=$2
+            shift
+            shift
+            ;;
+        --ncp-channel)
+            NCP_CHANNEL=$2
+            shift
+            shift
+            ;;
+        --network-name)
+            NETWORK_NAME=$2
+            shift
+            shift
+            ;;
+        --psck)
+            NETWORK_PSKC=$2
+            shift
+            shift
+            ;;
+
+        *)
+            shift
+            ;;
+        esac
+    done
+}
+
+parse_args "$@"
+
+[ -n "$NCP_PATH" ] || NCP_PATH="/dev/ttyACM0"
+[ -n "$TUN_INTERFACE_NAME" ] || TUN_INTERFACE_NAME="wpan0"
+[ -n "$AUTO_PREFIX_ROUTE" ] || AUTO_PREFIX_ROUTE=true
+[ -n "$AUTO_PREFIX_SLAAC" ] || AUTO_PREFIX_SLAAC=true
+[ -n "$NAT64_PREFIX" ] || NAT64_PREFIX="64:ff9b::/96"
+[ -n "$NETWORK_NAME" ] || NETWORK_NAME="OT-BR-MQTT_SN"
+# Use same default network configuration as the Nordic Semiconductor Raspberry PI
+# borderrouter (see /etc/border_router.conf on the RPI)
+[ -n "$NETWORK_KEY" ] || NETWORK_KEY="00112233445566778899AABBCCDDEEFF"
+[ -n "$NETWORK_PANID" ] || NETWORK_PANID="0xABCD"
+[ -n "$NETWORK_XPANID" ] || NETWORK_XPANID="DEAD00BEEF00CAFE"
+[ -n "$NCP_CHANNEL" ] || NCP_CHANNEL="11"
+[ -n "$NETWORK_PSKC" ] || NETWORK_PSKC="E00F739803E92CB42DAA7CCE1D2A394D"
+
+echo "NCP_PATH:" $NCP_PATH
+echo "TUN_INTERFACE_NAME:" $TUN_INTERFACE_NAME
+echo "NAT64_PREFIX:" $NAT64_PREFIX
+echo "AUTO_PREFIX_ROUTE:" $AUTO_PREFIX_ROUTE
+echo "AUTO_PREFIX_SLAAC:" $AUTO_PREFIX_SLAAC
+
+NAT64_PREFIX=${NAT64_PREFIX/\//\\\/}
+
+sed -i "s/^prefix.*$/prefix $NAT64_PREFIX/" /etc/tayga.conf
+sed -i "s/dns64.*$/dns64 $NAT64_PREFIX {};/" /etc/bind/named.conf.options
+
+echo "Config:NCP:SocketPath \"$NCP_PATH\"" > /etc/wpantund.conf
+echo "Config:TUN:InterfaceName $TUN_INTERFACE_NAME " >> /etc/wpantund.conf
+echo "Daemon:SetDefaultRouteForAutoAddedPrefix $AUTO_PREFIX_ROUTE" >> /etc/wpantund.conf
+echo "IPv6:SetSLAACForAutoAddedPrefix $AUTO_PREFIX_SLAAC" >> /etc/wpantund.conf
+
+echo "OTBR_AGENT_OPTS=\"-I $TUN_INTERFACE_NAME\"" > /etc/default/otbr-agent
+echo "OTBR_WEB_OPTS=\"-I $TUN_INTERFACE_NAME -p 80\"" > /etc/default/otbr-web
+
+echo "net.ipv6.conf.all.disable_ipv6=0" >> /etc/sysctl.conf
+echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
+echo "net.ipv4.conf.all.forwarding=1" >> /etc/sysctl.conf
+
+NAT64=1 DNS64=1 /app/borderrouter/script/server
+
+echo "Starting MQTT-SN Gateway"
+nohup 2>&1 /app/borderrouter/MQTT-SNGateway &
+
+###############################################################################
+# Atach to the Thread network.
+###############################################################################
+
+if !(wpanctl getprop Network:Name | grep -q "Network:Name = \"$NETWORK_NAME\""); then
+    wpanctl leave
+    sleep 1
+
+    wpanctl reset
+    sleep 2
+
+    wpanctl setprop Network:Key --data $NETWORK_KEY
+    wpanctl setprop Network:PANID $NETWORK_PANID
+    wpanctl setprop Network:XPANID $NETWORK_XPANID
+    wpanctl setprop NCP:Channel $NCP_CHANNEL
+    wpanctl setprop Network:Name $NETWORK_NAME
+    wpanctl setprop Network:PSKc $NETWORK_PSKC
+    wpanctl attach
+    sleep 3
+    wpanctl status
+fi
+
+while [ $? = 0 ]
+do
+    sleep 60
+done
